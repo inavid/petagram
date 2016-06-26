c98a8f5702c8e9c766c96dd6ec091bf758ef3fd5
Divani Fuentes <inavid.divarolas@gmail.com>
2016-06-26 18:05:15 -0500

    Se hacen modificaciones para usar recicler view en pantalla 1

--- app/src/main/java/com/dfuentes/petgram/MainActivity.java
+++ app/src/main/java/com/dfuentes/petgram/MainActivity.java
@@ -2,12 +2,48 @@ package com.dfuentes.petgram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Mascotas> mascotas;
+    private RecyclerView ListaMascotas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar miActionBar = (Toolbar) findViewById(R.id.miappbar);
+        setSupportActionBar(miActionBar);
+
+        ListaMascotas = (RecyclerView) findViewById(R.id.rvMascotas);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        ListaMascotas.setLayoutManager(llm);
+
+        inicializaMascotas();
+        inicializaAdaptador();
+    }
+
+    public void inicializaAdaptador(){
+        MascotaAdaptador adaptador = new MascotaAdaptador(mascotas, this);
+        ListaMascotas.setAdapter(adaptador);
+    }
+
+    public void inicializaMascotas(){
+        mascotas  = new ArrayList<Mascotas>();
+
+        mascotas.add(new Mascotas("putin", R.drawable.recurso_1, "1"));
+        mascotas.add(new Mascotas("machoman", R.drawable.recurso_2, "0"));
+        mascotas.add(new Mascotas("rasputio", R.drawable.recurso_3, "2"));
+        mascotas.add(new Mascotas("Aurelio", R.drawable.recurso_4, "5"));
+        mascotas.add(new Mascotas("Gordi", R.drawable.recurso_5, "5"));
     }
 }
---
+++ app/src/main/java/com/dfuentes/petgram/Mascotas.java
@@ -0,0 +1,41 @@
+package com.dfuentes.petgram;
+
+/**
+ * Created by inavid on 24/06/16.
+ */
+public class Mascotas {
+
+    private String Nombre;
+    private int Foto;
+    private String Rate;
+
+    public Mascotas(String nombre, int foto, String rate) {
+        this.Nombre = nombre;
+        this.Foto = foto;
+        this.Rate = rate;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public int getFoto() {
+        return Foto;
+    }
+
+    public void setFoto(int foto) {
+        Foto = foto;
+    }
+
+    public String getRate() {
+        return Rate;
+    }
+
+    public void setRate(String rate) {
+        Rate = rate;
+    }
+}
